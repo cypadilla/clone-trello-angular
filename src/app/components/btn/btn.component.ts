d8d17263f7052947d57b8d4228db7cc0c9e731e2
Cristian Padilla <cristian.padilla@dipzo.net>
2023-12-18 22:21:17 -0500

    update btn component and todo dialog

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -7,28 +7,43 @@ import { Component, Input, OnInit } from '@angular/core';
 export class BtnComponent implements OnInit {
 
   @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
-  @Input() color = 'primary';
+  @Input() color : 'success'|'primary'| 'danger' | 'light' = 'primary';
 
+  mapColors = {
+    success: {
+      'bg-success-700' :true,
+      'hover:bg-success-900' :true,
+      'focus:ring-success-300' :true,
+      'text-white': true
+    },
+    primary: {
+      'bg-primary-700' : true,
+      'hover:bg-primary-900' : true,
+      'focus:ring-primary-300' : true,
+      'text-white': true
+    },
+    danger: {
+      'bg-red-700' : true,
+      'hover:bg-red-900' : true,
+      'focus:ring-red-300' : true,
+      'text-white': true
+    },
+    light: {
+      'bg-gray-200' : true,
+      'hover:bg-gray-500' : true,
+      'focus:ring-gray-50' : true,
+      'text-white': true
+    }
+  }
   ngOnInit(): void {
 
   }
 
   get colors(){
-    return {
-      'text-white': this.color === 'success' || this.color === 'primary' || this.color === 'red',
-      'text-gray-700': this.color === 'gray-light',
-      'bg-success-700' : this.color === 'success',
-      'hover:bg-success-900' : this.color === 'success',
-      'focus:ring-success-300' : this.color === 'success',
-      'bg-primary-700' : this.color === 'primary',
-      'hover:bg-primary-900' : this.color === 'primary',
-      'focus:ring-primary-300' : this.color === 'primary',
-      'bg-red-700' : this.color === 'red',
-      'hover:bg-red-900' : this.color === 'red',
-      'focus:ring-red-300' : this.color === 'red',
-      'bg-gray-200' : this.color === 'gray-light',
-      'hover:bg-gray-500' : this.color === 'gray-light',
-      'focus:ring-gray-50' : this.color === 'gray-light'
-    }
+    const colors = this.mapColors[this.color];
+    if(colors) {
+      return colors;
+    };
+    return {}
   }
 }
